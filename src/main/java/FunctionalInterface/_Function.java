88ae583309dcf968aa0b3afd29a0af2ce8c996c6
George Gardner <Gpgardner@yahoo.com>
2021-09-07 14:01:57 -0400

    Functional and biFunctional functions

--- src/main/java/FunctionalInterface/_Function.java
+++ src/main/java/FunctionalInterface/_Function.java
@@ -1,4 +1,31 @@
 package FunctionalInterface;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class _Function {
+
+    public static void main(String[] args) {
+        int number = 1;
+
+        int number2 = incrementByOneFunction.apply(number);
+        System.out.println(number2);
+
+        Function<Integer, Integer> add1AndThenMultiplyBy10 = incrementByOneFunction
+                .andThen(multiplyBy10Function);
+
+        System.out.println(incrementByOneAndMultiplyByAValue.apply(2, 10));
+
+
+    }
+
+    //functions
+    static Function<Integer, Integer> incrementByOneFunction = number -> ++number;
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    //bifunctions
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyByAValue =
+            (numberToIncrementByOne, numberToMultiplyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
 }
